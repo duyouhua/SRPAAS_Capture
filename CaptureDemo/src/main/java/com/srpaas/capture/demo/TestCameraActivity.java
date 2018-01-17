c9cd54c3b25e30600ea861e4069afecc450ed713
dingna <dingn@suirui.com>
2018-01-17 15:25:11 +0800

    优化

--- CaptureDemo/src/main/java/com/srpaas/capture/demo/TestCameraActivity.java
+++ CaptureDemo/src/main/java/com/srpaas/capture/demo/TestCameraActivity.java
@@ -1,22 +1,17 @@
 package com.srpaas.capture.demo;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.Surface;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
 
-import com.srpaas.capture.render.CameraInterface;
+import com.srpaas.capture.render.CameraGLSurfaceView;
+import com.srpaas.capture.render.CameraRender;
 import com.srpaas.capture.service.VideoService;
 import com.srpaas.capture.service.VideoServiceImpl;
 import com.srpaas.capture.service.VideoServiceListener;
-
-import java.lang.reflect.Method;
+import com.srpaas.capture.util.ToolUtil;
 
 /**
  * @authordingna
@@ -26,28 +21,47 @@ public class TestCameraActivity extends Activity implements VideoServiceListener
 
     private VideoService videoService;
     private MyOrientationDetector myOrientationDetector;
+    private CameraGLSurfaceView mBigCameraView, mSmallCameraView;
+    private CameraRender mBigCameraRender, mSmallCameraRender;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_layout);
-        FrameLayout bigFrameLayout = (FrameLayout) findViewById(R.id.bigLayout);
-        CameraSurFaceView cameraSurFaceView = new CameraSurFaceView(this);
-        bigFrameLayout.addView(cameraSurFaceView.getView());
-
+        findView();
         videoService = new VideoServiceImpl();
         videoService.addVideoServiceListener(this);
         videoService.startCapture(this, 1);
     }
 
-    private static int getScreenRotation(Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        try {
-            Method m = display.getClass().getDeclaredMethod("getRotation");
-            return (Integer) m.invoke(display);
-        } catch (Exception e) {
-            return Surface.ROTATION_0;
-        }
+    private void findView() {
+        //big video
+        FrameLayout bigFrameLayout = (FrameLayout) findViewById(R.id.bigLayout);
+        CameraSurFaceView mBigCamera = new CameraSurFaceView(this);
+        addCameraView(bigFrameLayout, mBigCamera, false);
+
+        //small video
+        FrameLayout smallFrameLayout = (FrameLayout) findViewById(R.id.smallLayout);
+        CameraSurFaceView mSmallCamera = new CameraSurFaceView(this);
+        addCameraView(smallFrameLayout, mSmallCamera, true);
+    }
+
+    private void addCameraView(FrameLayout frameLayout, CameraSurFaceView cameraView, final boolean isMediaOverlay) {
+        cameraView.addItemView(new CameraSurFaceView.getItemView() {
+            @Override
+            public void onItemView(CameraGLSurfaceView cameraGLSurfaceView, CameraRender cameraRender) {
+                cameraGLSurfaceView.setZOrderMediaOverlay(isMediaOverlay);
+                if (!isMediaOverlay) {
+                    mBigCameraView = cameraGLSurfaceView;
+                    mBigCameraRender = cameraRender;
+                } else {
+                    mSmallCameraView = cameraGLSurfaceView;
+                    mSmallCameraRender = cameraRender;
+                }
+            }
+        });
+        frameLayout.addView(cameraView.getView());
     }
 
     @Override
@@ -98,48 +112,10 @@ public class TestCameraActivity extends Activity implements VideoServiceListener
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        getScreenOrientation(this);
-        updateOnConfigurationChanged();
+        boolean isScreen = ToolUtil.getScreenOrientation(this);
+        updateOnConfigurationChanged(isScreen);
     }
 
-    public boolean getScreenOrientation(Context context) {
-        int mRotation = getOrientation(context);
-        //设置当前屏幕的方向/角度
-        if (mRotation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
-            CameraInterface.getInstance().setRotation(0);
-            return false;
-        } else if (mRotation == ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) {
-            CameraInterface.getInstance().setRotation(2);
-            return false;
-        } else if (mRotation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
-            CameraInterface.getInstance().setRotation(1);
-            return true;
-        } else if (mRotation == ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE) {
-            CameraInterface.getInstance().setRotation(3);
-            return true;
-        } else {
-            CameraInterface.getInstance().setRotation(0);//默认是竖屏
-            return false;
-        }
-    }
-
-    //获取当前屏幕方向
-    private int getOrientation(Context context) {
-        switch (getScreenRotation(context)) {
-            case Surface.ROTATION_0:
-                return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-            case Surface.ROTATION_90:
-                return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-            case Surface.ROTATION_180:
-                return (Build.VERSION.SDK_INT >= 8 ? ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
-                        : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-            case Surface.ROTATION_270:
-                return (Build.VERSION.SDK_INT >= 8 ? ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
-                        : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-            default:
-                return 0;
-        }
-    }
 
     @Override
     public void onstartCaptureFailListener() {
@@ -165,7 +141,7 @@ public class TestCameraActivity extends Activity implements VideoServiceListener
     public void onScreenChange(int oldRotation, int rotation) {
         if (rotation == 0 || rotation == 2) {
             if (oldRotation == 0 || oldRotation == 2) {
-                updateOnConfigurationChanged();
+                updateOnConfigurationChanged(false);
             } else {
                 if (rotation == 0) {
                     this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//竖屏
@@ -175,7 +151,7 @@ public class TestCameraActivity extends Activity implements VideoServiceListener
             }
         } else {
             if (oldRotation == 1 || oldRotation == 3) {
-                updateOnConfigurationChanged();
+                updateOnConfigurationChanged(true);
             } else {
                 if (rotation == 1) {
                     this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//横屏
@@ -186,7 +162,31 @@ public class TestCameraActivity extends Activity implements VideoServiceListener
         }
     }
 
-    private void updateOnConfigurationChanged() {
+    private void updateOnConfigurationChanged(boolean isScreen) {
         //屏幕旋转成功了，开始做旋转之后的逻辑
+        if (mBigCameraView != null && mBigCameraRender != null) {
+            updateCameraVideo(mBigCameraView, mBigCameraRender, isScreen);
+        }
+        if (mSmallCameraView != null && mSmallCameraRender != null) {
+            updateCameraVideo(mSmallCameraView, mSmallCameraRender, isScreen);
+        }
+    }
+
+    private void updateCameraVideo(CameraGLSurfaceView cameraView, CameraRender cameraRender, boolean isScreen) {
+        int width = cameraView.getMeasuredWidth();
+        int height = cameraView.getMeasuredHeight();
+        if (isScreen) {//横屏
+            if (width > height) {
+                cameraRender.updateLocalVideo(width, height);
+            } else {
+                cameraRender.updateLocalVideo(height, width);
+            }
+        } else {//竖屏
+            if (width < height) {
+                cameraRender.updateLocalVideo(width, height);
+            } else {
+                cameraRender.updateLocalVideo(height, width);
+            }
+        }
     }
 }
